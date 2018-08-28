2a7cc6c75ee153216cacb8954cd9096baa663092
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 16:27:51 -0300

    Saved grace/__log__.py at Timestamp: 2018-08-28 19:27:50

--- grace/__log__.py
+++ grace/__log__.py
@@ -22,3 +22,21 @@ SyntaxError: EOL while scanning string literal
     Tesouro ()
 TypeError: Tesouro() missing 1 positional argument: inca
 '''},
+{'date': 'Tue Aug 28 2018 16:27:28.75 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    Tesouro ()
+  module <module> line 21
+    templotras3= Cena(img= TEMPLOTRAS3)
+TypeError: 'module' object is not callable
+'''},
