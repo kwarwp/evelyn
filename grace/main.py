827d58fa3a291b2f636ba8bfdb9c7c2ef470459a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 14:53:16 -0300

    Saved grace/main.py at Timestamp: 2018-08-28 17:53:15

--- grace/main.py
+++ grace/main.py
@@ -1,3 +1,9 @@
 # evelyn.grace.main.py
 from spy.vitollino.main import Cena, Elemento
 from _spy.vitollino.main import INVENTARIO as inv
+ 
+PEDRAS1 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_102220.jpg"
+PEDRAS2 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_105412.jpg"
+PEDRAS3 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_105419.jpg"
+PEDRAS4 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_105624.jpg"
+TEMPLOTRAS1 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_110147.jpg"
