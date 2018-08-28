193538cab855bbcb74ba7d570db9f2812eeaca73
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 15:24:20 -0300

    Created grace/__log__.py at Timestamp: 2018-08-28 18:24:20

---
+++ grace/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 28 2018 15:23:56.518 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  ARTEFATOS1 = "Introducao_a_Computacao/Untitled_20180828_105529.jpg
+                                                                    ^
+SyntaxError: EOL while scanning string literal
+'''},
