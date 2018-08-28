9e824f541de2b3ffee9260c6cf95e77feced0313
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 15:13:28 -0300

    Saved grace/main.py at Timestamp: 2018-08-28 18:13:28

--- grace/main.py
+++ grace/main.py
@@ -2,11 +2,26 @@
 from spy.vitollino.main import Cena, Elemento
 from _spy.vitollino.main import INVENTARIO as inv
  
-PEDRAS1 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_102220.jpg"
-PEDRAS2 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_105412.jpg"
-PEDRAS3 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_105419.jpg"
-PEDRAS4 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_105624.jpg"
+PEDRAS1 = "Introducao_a_Computacao/Untitled_20180828_102220.jpg"
+PEDRAS2 = "Introducao_a_Computacao/Untitled_20180828_105412.jpg"
+PEDRAS3 = "Introducao_a_Computacao/Untitled_20180828_105419.jpg"
+PEDRAS4 = "Introducao_a_Computacao/Untitled_20180828_105624.jpg"
 TEMPLOTRAS1 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_110147.jpg"
 TEMPLOTRAS2 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_110148.jpg"
 TEMPLOTRAS3 = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_110145.jpg"
 CARTASENTRAESAI = "https://activufrj.nce.ufrj.br/studio/info/Introducao_a_Computacao/Untitled_20180828_105727.jpg"
+	
+0	
+1	
+2	
+3	"Introducao_a_Computacao/Untitled_20180828_105528.jpg"
+4	"Introducao_a_Computacao/Untitled_20180828_105529.jpg"
+5	"Introducao_a_Computacao/Untitled_20180828_105623.jpg"
+6	
+7	"Introducao_a_Computacao/Untitled_20180828_105627.jpg"
+8	"Introducao_a_Computacao/Untitled_20180828_105727.jpg"
+9	"Introducao_a_Computacao/Untitled_20180828_105833-0.jpg"
+10	"Introducao_a_Computacao/Untitled_20180828_105833-2.jpg"
+11	"Introducao_a_Computacao/Untitled_20180828_110145.jpg"
+12	"Introducao_a_Computacao/Untitled_20180828_110147.jpg"
+13	"Introducao_a_Computacao/Untitled_20180828_110148.jpg"
