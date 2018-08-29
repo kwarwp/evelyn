1a43fbfde5268dd3082afd34dbc04a0188b8af7c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:01:49 -0300

    Created libby/__log__.py at Timestamp: 2018-08-29 14:01:49

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 29 2018 11:01:47.116 GMt-0300 (Hora oficial do Brasil) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  tarzan = Elemento(img=TARZAN, tit="Tarzan", style=dict(left="100px", top="160px, width="60px", height="200px"))
+                                                                                           ^
+SyntaxError: invalid syntax
+'''},
