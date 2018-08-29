813c258b385ecc49c3de6c992f3a6d67dd049205
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 09:38:10 -0300

    Saved roxanne/main.py at Timestamp: 2018-08-29 12:38:10

--- roxanne/main.py
+++ roxanne/main.py
@@ -8,11 +8,11 @@ TEMPLOTRAS2 = "Introducao_a_Computacao/Untitled_20180828_110148.jpg",
 TEMPLOTRAS3 = "Introducao_a_Computacao/Untitled_20180828_110145.jpg"
 )
 PERSONAGENS = dict(
-CARTASENTRAESAI = "Introducao_a_Computacao/Untitled_20180828_105727.jpg",
-CARTASTRAS = "Introducao_a_Computacao/Untitled_20180828_105833-2.jpg",
-ARTEFATOS1 = "Introducao_a_Computacao/Untitled_20180828_105529.jpg",
-ARTEFATOS2 = "Introducao_a_Computacao/Untitled_20180828_105528.jpg",
-MOSTROS1 = "Introducao_a_Computacao/Untitled_20180828_105623.jpg",
+ALICE = "https://i.imgur.com/wLTlDgD.png",
+BARBIE = "https://i.imgur.com/a3oxZpW.png",
+HOMEMARANHA = "https://i.imgur.com/q4YqcdK.png",
+MESTRE = "https://i.imgur.com/wbOu85i.png",
+CINDERELA = "https://i.imgur.com/8YSQS00.png",
 MOSTROS = "Introducao_a_Computacao/Untitled_20180828_105627.jpg"
 )
 TEMPLOTRAS1 = "Introducao_a_Computacao/Untitled_20180828_110147.jpg",
