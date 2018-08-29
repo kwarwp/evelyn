42585ae6a8e211cd4c1dcf3911366ab6d32987e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:02:04 -0300

    Created ruzwana/__log__.py at Timestamp: 2018-08-29 14:02:03

---
+++ ruzwana/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 29 2018 11:02:00.378 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  textocastelo = Texto (castelo, "Tarzan encontra jesuítas na floresta que tentam catequisá-lo. Devido a sua recusa, foi levado por eles à Europa para tentarem novamente. Dessa vez, imerso na cultura européia e hospedado em um castelo.
+                                                                                                                                                                                                                                           ^
+SyntaxError: EOL while scanning string literal
+'''},
