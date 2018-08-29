b6d7fe4344ebceba4aa4c9a8745efe0c0da14373
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:44:23 -0300

    Saved stacy/__log__.py at Timestamp: 2018-08-29 14:44:23

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -140,3 +140,23 @@ NameError: name 'ebeijo' is not defined
     fim.vai = efim.vai
 NameError: name 'efim' is not defined
 '''},
+{'date': 'Wed Aug 29 2018 11:44:23.155 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 51
+    criarcenas()
+  module <module> line 48
+    fim.entra(FIM)
+  module _spy.vitollino.main line 456
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'str' object has no attribute 'nome'
+'''},
