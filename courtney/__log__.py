7717228dee648b3d54cb3f514eebb469266e7762
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:49:30 -0300

    Saved courtney/__log__.py at Timestamp: 2018-08-29 14:49:30

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -171,3 +171,21 @@ SyntaxError: invalid syntax
     alice.vai = ealice.vai
 NameError: name 'ealice' is not defined
 '''},
+{'date': 'Wed Aug 29 2018 11:49:29.464 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    criarcenas()    
+  module <module> line 32
+    selva.mesa.vai()
+AttributeError: 'Cena' object has no attribute 'mesa'
+'''},
