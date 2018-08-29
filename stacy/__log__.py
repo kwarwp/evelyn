915657785255cbe57eca1dc0ce68d5fe15fc5b37
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 23:36:01 -0300

    Saved stacy/__log__.py at Timestamp: 2018-08-29 02:36:01

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -6,3 +6,7 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Tue Aug 28 2018 23:36:01.281 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <SyntaxError: Unexpected token {>
+'''},
