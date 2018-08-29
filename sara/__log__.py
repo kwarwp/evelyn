3102782c80934ec168d266234516aa6700051106
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 23:38:35 -0300

    Created sara/__log__.py at Timestamp: 2018-08-29 02:38:35

---
+++ sara/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 28 2018 23:38:33.324 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  castelo = Cena(img=CASTELO)
+  ^
+IndentationError: expected an indented block
+'''},
