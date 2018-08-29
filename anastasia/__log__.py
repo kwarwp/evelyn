854198081ec9ade02085bd5253bf349332e47794
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 10:07:19 -0300

    Created anastasia/__log__.py at Timestamp: 2018-08-29 13:07:19

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 29 2018 10:07:18.440 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 27
+  Jogo()
+  ^
+IndentationError: expected an indented block
+'''},
