fa579ee2d4b81d517677d53348a40705f6dfddaf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:17:08 -0300

    Saved courtney/__log__.py at Timestamp: 2018-08-29 14:17:08

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -71,3 +71,21 @@ AttributeError: 'Elemento' object has no attribute 'main'
     tarzan = Elemento(img=TARZAN_NA_SELVA, tit="Tarzan", style=dict(left="150px", top="150px", width="150px", heigth="150px")).main.py
 AttributeError: 'Elemento' object has no attribute 'main'
 '''},
+{'date': 'Wed Aug 29 2018 11:17:07.653 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    criarcenas()
+  module <module> line 18
+    tarzan = Elemento(img=TARZAN, tit="Tarzan", style=dict(left="150px", top="150px", width="150px", heigth="150px")).main.py
+NameError: name 'TARZAN' is not defined
+'''},
