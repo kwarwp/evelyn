0741d70b5b4c0ac3f856a5642346e09a0e579764
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 23:27:55 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-08-29 02:27:55

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -34,3 +34,10 @@ SyntaxError: invalid syntax
                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Aug 28 2018 23:27:55.220 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  City = Cena (img=CITY)
+  ^
+IndentationError: expected an indented block
+'''},
