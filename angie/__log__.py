84be277bc5ba47609f852762afed7b19d25f089a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:08:06 -0300

    Created angie/__log__.py at Timestamp: 2018-08-29 14:08:06

---
+++ angie/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 29 2018 11:08:06.203 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 40
+  ecinderela = Texto(central_park, "SIM, me ajude! É minha amiga Barbie, ela está perdida na parte assombrada."
+                    ^
+SyntaxError: Unbalanced bracket (
+'''},
