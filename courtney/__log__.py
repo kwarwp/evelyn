2ae4f0b24370aa5e8485c1fb741495259f367445
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:17:50 -0300

    Saved courtney/__log__.py at Timestamp: 2018-08-29 14:17:50

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -89,3 +89,21 @@ AttributeError: 'Elemento' object has no attribute 'main'
     tarzan = Elemento(img=TARZAN, tit="Tarzan", style=dict(left="150px", top="150px", width="150px", heigth="150px")).main.py
 NameError: name 'TARZAN' is not defined
 '''},
+{'date': 'Wed Aug 29 2018 11:17:49.633 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 24
+    criarcenas()
+  module <module> line 20
+    etarzan = texto (selva, "Tarzan, que foi criado na selva, quando chega na idade adulta, por conhecer a selva muito bem e se achar o rei dela se perde na floresta.")
+NameError: name 'texto' is not defined
+'''},
