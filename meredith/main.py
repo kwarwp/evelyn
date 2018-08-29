ffe449d3108b6223ae59e2f07ac18966f0e2e244
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:01:07 -0300

    Saved meredith/main.py at Timestamp: 2018-08-29 14:01:07

--- meredith/main.py
+++ meredith/main.py
@@ -3,7 +3,7 @@ from _spy.vitollino.main import Elemento, Cena, Texto
 from _spy.vitollino.main import INVENTARIO as inv
 
 BARBIE = "https://i.pinimg.com/originals/9d/85/6c/9d856c0168614e8d429ba0c6bb01d0f6.png"
-ALICE = "https://3.bp.blogspot.com/-o7Y78sYGkjY/V6O1G7WysSI/AAAAAAAAMO8/IG0Q7cJKKcYA70fLNINSaLG02t9fQT52QCLcB/s1600/ALICE%2B%25283%2529.png'
+ALICE = "https://3.bp.blogspot.com/-o7Y78sYGkjY/V6O1G7WysSI/AAAAAAAAMO8/IG0Q7cJKKcYA70fLNINSaLG02t9fQT52QCLcB/s1600/ALICE%2B%25283%2529.png"
 
 FLORESTA = "https://3.bp.blogspot.com/-w42aVYJVVps/TicGHsUvjrI/AAAAAAAACxQ/KlDH3JScanE/s1600/pequeno-caminho-na-floresta-imagens-imagem-de-fundo-wallpaper-para-pc-computador-tela-gratis-ambiente-de-trabalho.jpg"
 CASTELO = "https://img3.stockfresh.com/files/d/ddraw/m/85/4928415_stock-vector-mythological-landscape-with-medieval-castle.jpg"
@@ -18,3 +18,7 @@ def criarcenas():
     ealice = Texto (floresta, "Que floresta linda!")
     alice.vai = ealice.vai
 
+
+    floresta.vai()
+    
+criarcenas()
