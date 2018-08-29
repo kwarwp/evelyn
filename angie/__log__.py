c00caf5c830024846720641fdb837fba661e390e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:12:23 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-29 14:12:23

--- angie/__log__.py
+++ angie/__log__.py
@@ -6,3 +6,10 @@
                     ^
 SyntaxError: Unbalanced bracket (
 '''},
+{'date': 'Wed Aug 29 2018 11:12:24.843 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 55
+  floresta.vai()
+  ^
+IndentationError: unexpected indent
+'''},
