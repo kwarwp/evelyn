9b9080b4754dfa41559b034f47145b8e9a1a87e6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 23:25:58 -0300

    Saved roxanne/__log__.py at Timestamp: 2018-08-29 02:25:57

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Aug 28 2018 23:25:57.342 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  def criarcenas ()
+                    ^
+SyntaxError: invalid syntax
+'''},
