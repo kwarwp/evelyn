3acfbe0db4f3a05ffa1ab8e747c21cbdde31c487
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 23:34:55 -0300

    Created stacy/__log__.py at Timestamp: 2018-08-29 02:34:55

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 28 2018 23:34:52.937 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  City = Cena (img=CITY)
+  ^
+IndentationError: expected an indented block
+'''},
