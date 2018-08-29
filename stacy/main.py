78eaefda61929712e003e4a92c59ec264ee10619
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:51:44 -0300

    Saved stacy/main.py at Timestamp: 2018-08-29 14:51:43

--- stacy/main.py
+++ stacy/main.py
@@ -18,6 +18,7 @@ def criarcenas():
     bcastelo = Cena(img=CASTELO)
     fim = Cena(img=FIM)    
     floresta.direita = castelo
+    castelo.direita = ecastelo
     
     alice = Elemento(img=ALICE, tit="Alice", style=dict(left="100px",top="160",width="60px",heigth="200px"))
     alice.entra(floresta)
