d7a7d454e096db1f9a239c15b270035cd9aff9a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 10:57:40 -0300

    Created parisa/__log__.py at Timestamp: 2018-08-29 13:57:40

---
+++ parisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 29 2018 10:57:38.613 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 19
+  "cinderela.entra(laboratorioa)
+                                ^
+SyntaxError: EOL while scanning string literal
+'''},
