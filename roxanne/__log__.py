8f064b8556349de76ac2e8d3d3605cc8f6793af0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-28 23:23:25 -0300

    Created roxanne/__log__.py at Timestamp: 2018-08-29 02:23:25

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Tue Aug 28 2018 23:23:22.969 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  def criarcenas ()
+                    ^
+SyntaxError: invalid syntax
+'''},
