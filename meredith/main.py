0814b430fde279f4c719380f229c08de2f35de7c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 09:41:40 -0300

    Saved meredith/main.py at Timestamp: 2018-08-29 12:41:40

--- meredith/main.py
+++ meredith/main.py
@@ -1,3 +1,12 @@
 # evelyn.meredith.main.py
 from _spy.vitollino.main import Elemento, Cena, Texto
 
+bgCena1 = "https://i1.wp.com/salmeninsurance.com/home/wp-content/uploads/2016/03/bg-grunge-1.jpg"
+
+def Jogo():
+    cena1 = Cena(img = bgCena1)
+    cena1.vai()
+    
+    
+Jogo()
+
