cbfe33f32de749c81ea7e61bd20a134727bf3d84
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 10:19:12 -0300

    Saved stacy/main.py at Timestamp: 2018-08-29 13:19:12

--- stacy/main.py
+++ stacy/main.py
@@ -6,7 +6,3 @@ ALICE ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad
 TARZAN ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiL6qncppLdAhXCH5AKHZAyBUMQjRx6BAgBEAU&url=http%3A%2F%2Fhero.wikia.com%2Fwiki%2FFile%3ATarzan_Character.png&psig=AOvVaw2Frm4u_vk1-RCkiipJI9_5&ust=1535633480343303"
 FLORESTA ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=2ahUKEwiCzvyAq5LdAhXBFJAKHeuDBh8QjRx6BAgBEAU&url=https%3A%2F%2Falgarve-saibamais.blogspot.com%2F2016%2F03%2Fcomo-o-vento-na-floresta-poema-de.html&psig=AOvVaw392PmZLmsaLorhggTmv0pB&ust=1535634513603880"
 CASTELO ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjdwPnPq5LdAhVGmJAKHUF6BQ0QjRx6BAgBEAU&url=http%3A%2F%2Fwww.fazarte.com.br%2Fhome%2Fproduto%2F60-Castelo-Da-Floresta.html&psig=AOvVaw1JIb2JL8P5HQ8KN0n-ZF1h&ust=1535634776117423"
-
-def criarcenas():
-    floresta = Cena(img=FLORESTA)
-    castelo = Cena(img=CASTELO)
