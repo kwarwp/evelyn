e1c31d14bff8ccd337934299c24b79c1dbbd3dc6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 09:39:03 -0300

    Created meredith/__log__.py at Timestamp: 2018-08-29 12:39:03

---
+++ meredith/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Aug 29 2018 09:39:02.767 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 5
+  bgCena2 =
+            ^
+SyntaxError: invalid syntax
+'''},
