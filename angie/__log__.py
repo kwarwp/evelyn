590c12a6ef51253860b912ad3284b1b3ea769952
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:30:32 -0300

    Saved angie/__log__.py at Timestamp: 2018-08-29 14:30:32

--- angie/__log__.py
+++ angie/__log__.py
@@ -203,3 +203,21 @@ SyntaxError: EOL while scanning string literal
                                                                                                                 ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Wed Aug 29 2018 11:30:31.822 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    criarcenas()
+  module <module> line 16
+    barbie = Elemento(img=barbie, tit="barbie", style=dict(left="100px", top="160px", width="60px", height="200px" ))
+UnboundLocalError: local variable 'barbie' referenced before assignment
+'''},
