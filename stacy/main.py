c320a804349eb7d3bcdaa0fe43e3255f29ba8f87
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 11:13:15 -0300

    Saved stacy/main.py at Timestamp: 2018-08-29 14:13:14

--- stacy/main.py
+++ stacy/main.py
@@ -8,6 +8,7 @@ FLORESTA ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&
 CASTELO ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjdwPnPq5LdAhVGmJAKHUF6BQ0QjRx6BAgBEAU&url=http%3A%2F%2Fwww.fazarte.com.br%2Fhome%2Fproduto%2F60-Castelo-Da-Floresta.html&psig=AOvVaw1JIb2JL8P5HQ8KN0n-ZF1h&ust=1535634776117423"
 PIANO ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi2sMu_sJLdAhVEFJAKHUOmDeoQjRx6BAgBEAU&url=http%3A%2F%2Fescolademusicalumiere.blogspot.com%2Fp%2Fcursos.html&psig=AOvVaw0MVfMyD7JnHHYzMU9nzCv-&ust=1535636086717191"
 BEIJO ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj75eOfsZLdAhUIipAKHVb4AjkQjRx6BAgBEAU&url=http%3A%2F%2Farimaamtricaapoesia.blogspot.com%2F&psig=AOvVaw0y0toAjNn_zRqdMviDkuyV&ust=1535636286353318"
+FIM = "https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwivw4acuJLdAhUFjpAKHUB3DesQjRx6BAgBEAU&url=http%3A%2F%2Fmagnitudesdomundo.blogspot.com%2F2013%2F06%2Fdia-dos-namorados-12-de-junho.html&psig=AOvVaw04zkigDpwUyOfML9dxkeyn&ust=1535638097834402"
 
 def criarcenas():
     floresta = Cena(img=FLORESTA)
@@ -39,6 +40,8 @@ def criarcenas():
     beijo = Texto(castelo, "Os dois se apaixonam e...")
     beijo.vai = ebeijo.vai
     
-    
+    fim = Elemento(img=FIM, tit="Fim", style=dict(left="100px",top="160",width="60px",heigth="200px"))
+    fim.entra(castelo)
+    fim.vai = efim.vai
     
     
