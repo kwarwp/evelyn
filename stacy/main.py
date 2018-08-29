a27cf700e562f242081e3ac4404b17a0eaffb093
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-29 10:05:55 -0300

    Saved stacy/main.py at Timestamp: 2018-08-29 13:05:54

--- stacy/main.py
+++ stacy/main.py
@@ -3,3 +3,4 @@ from _spy.vitollino.main import Cena, Texto, Elemento
 from _spy.vitollino.main import INVENTARIO as inv
 
 ALICE ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj9sKDTqJLdAhVFh5AKHf9rBfIQjRx6BAgBEAU&url=http%3A%2F%2Ffabricadesonhosfsa.blogspot.com%2F2016%2F06%2Fpng-alice-no-pais-das-maravilhas.html&psig=AOvVaw0Fkq-N9nsM7TJB0ucAB0ic&ust=1535633979340667"
+TARZAN ="https://www.google.com.br/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiL6qncppLdAhXCH5AKHZAyBUMQjRx6BAgBEAU&url=http%3A%2F%2Fhero.wikia.com%2Fwiki%2FFile%3ATarzan_Character.png&psig=AOvVaw2Frm4u_vk1-RCkiipJI9_5&ust=1535633480343303"
