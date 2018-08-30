942dd5a81a9da4706c3699434f3e342e42e1c617
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 20:03:11 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-30 23:03:11

--- danae/__log__.py
+++ danae/__log__.py
@@ -113,3 +113,19 @@ TypeError: 'int' object is not iterable
     self.sprite = Sprite(mesa.acampamento, jogador)
 AttributeError: 'Mesa' object has no attribute 'acampamento'
 '''},
+{'date': 'Thu Aug 30 2018 19:59:29.818 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 72
+    SPRITES = {key: Sprite(img, ind, tit=key) for key, (img, ind) in SPRITES.items()}
+TypeError: __init__() got multiple values for argument 'tit'
+'''},
