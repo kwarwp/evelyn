d708ebd3521f10b36faead8d0a3a7707dd61d0bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 19:53:55 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-30 22:53:55

--- danae/__log__.py
+++ danae/__log__.py
@@ -35,3 +35,21 @@ TypeError: 'module' object is not callable
     SPRITES = {key: Sprite(*args) for key, *args in SPRITES.items()}
 TypeError: __init__ missing 1 positional argument: 'img'
 '''},
+{'date': 'Thu Aug 30 2018 19:50:13.255 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 72
+    SPRITES = {key: Sprite(img, ind) for key, (img, ind) in SPRITES.items()}
+  module <module> line 67
+    super().__init__(IMGS[img], tit=img, style=dict(
+KeyError: http://activufrj.nce.ufrj.br/studio/Introducao_a_Computacao/Untitled_20180828_102220.jpg?disp=inline&size=G
+'''},
