62bf386ca6dc3bd64096f4d5913f490a3b05eabd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 20:05:41 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-30 23:05:41

--- danae/__log__.py
+++ danae/__log__.py
@@ -151,3 +151,27 @@ TypeError: __init__() got multiple values for argument 'tit'
     self.sprite.entra(mesa.acampamento)
 AttributeError: 'Mesa' object has no attribute 'acampamento'
 '''},
+{'date': 'Thu Aug 30 2018 20:01:59.134 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 229
+    Jogo().inicia()
+  module <module> line 222
+    self.mesa = Mesa(jogadores)
+  module <module> line 185
+    self.jogadores_ativos = self.jogadores = [Jogador(jogador, self) for jogador in jogadores]
+  module <module> line 156
+    self.sprite.entra(mesa.acampamento)
+  module _spy.vitollino.main line 456
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'Elemento' object has no attribute 'nome'
+'''},
