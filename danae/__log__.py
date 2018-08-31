1dfb1fb22b7cc70d80f7b249a55fdee85fc6d835
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 21:26:07 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-31 00:26:07

--- danae/__log__.py
+++ danae/__log__.py
@@ -407,3 +407,29 @@ IndexError: pop index out of range
     jogador.recebe(self.valor // cota)
 ZeroDivisionError: division by zero
 '''},
+{'date': 'Thu Aug 30 2018 21:22:25.67 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 227
+    Jogo().inicia()
+  module <module> line 223
+    self.mesa.inicia()
+  module <module> line 187
+    self.rodada(artefato)
+  module <module> line 194
+    while self.turno():
+  module <module> line 212
+    if jogador.joga() == DESISTE:
+  module <module> line 161
+    return self.chance.pop()
+IndexError: pop index out of range
+'''},
