c2372672a45be5229bd3ba9d54cef380c92fcaab
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-08-30 21:03:51 -0300

    Saved danae/__log__.py at Timestamp: 2018-08-31 00:03:51

--- danae/__log__.py
+++ danae/__log__.py
@@ -277,3 +277,29 @@ KeyError: 1
     for jogador in jogadores:
 TypeError: 'Jogador' object is not iterable
 '''},
+{'date': 'Thu Aug 30 2018 21:00:09.679 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 233
+    Jogo().inicia()
+  module <module> line 229
+    self.mesa.inicia()
+  module <module> line 195
+    self.rodada(artefato)
+  module <module> line 202
+    while self.turno():
+  module <module> line 220
+    carta.premia(jogador)
+  module <module> line 123
+    for jogador in jogadores:
+NameError: name 'jogadores' is not defined
+'''},
