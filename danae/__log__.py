9da6c8c7a8858aeaeacb8f0fdfa65318e0f2b04c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-01 20:15:49 -0300

    Saved danae/__log__.py at Timestamp: 2018-09-01 23:15:49

--- danae/__log__.py
+++ danae/__log__.py
@@ -702,3 +702,19 @@ AttributeError: 'Sprite' object has no attribute 'nome'
     self.scorer = dict(ponto=1, valor=cena.nome, carta=img, casa=self.xy, move=None)
 AttributeError: 'Sprite' object has no attribute 'nome'
 '''},
+{'date': 'Sat Sep 01 2018 20:15:48.637 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    STYLE["width"] = 800
+AttributeError: 'module' object has no attribute '__setitem__'
+'''},
