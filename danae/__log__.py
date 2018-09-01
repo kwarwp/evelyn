1362158f4d4bd36f9f3e1e20bf4c8059c8e45111
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-01 12:22:35 -0300

    Saved danae/__log__.py at Timestamp: 2018-09-01 15:22:35

--- danae/__log__.py
+++ danae/__log__.py
@@ -433,3 +433,29 @@ ZeroDivisionError: division by zero
     return self.chance.pop()
 IndexError: pop index out of range
 '''},
+{'date': 'Sat Sep 01 2018 12:22:34.46 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 232
+    Jogo().inicia()
+  module <module> line 224
+    self.mesa = Mesa(jogadores)
+  module <module> line 185
+    self.baralho = Baralho()
+  module <module> line 129
+    self.monta_baralho()
+  module <module> line 143
+    self.cartas.append(Tesouro(face=tesouro))
+  module <module> line 85
+    self.elt = SPRITES[face if face.isalpha() else "t{}".format(face)]
+KeyError: t14
+'''},
