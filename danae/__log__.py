ebf471d0c8e1961dfa72d52a439cd36a9205ba5f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-01 18:31:04 -0300

    Saved danae/__log__.py at Timestamp: 2018-09-01 21:31:04

--- danae/__log__.py
+++ danae/__log__.py
@@ -572,3 +572,25 @@ KeyError: img
     imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
 KeyError: img
 '''},
+{'date': 'Sat Sep 01 2018 18:31:03.49 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 271
+    Jogo().inicia()
+  module <module> line 264
+    self.mesa = Mesa(jogadores)
+  module <module> line 202
+    self.mesa = Cenario(IMGS["TEMPLOTRAS3"], 0, 800)
+  module <module> line 85
+    self.etl.style.width = w
+AttributeError: 'Cenario' object has no attribute 'etl'
+'''},
