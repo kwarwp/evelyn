67c209a406258c16ffa56a86f2a9f1b682abaeb5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-09-01 20:10:44 -0300

    Saved danae/__log__.py at Timestamp: 2018-09-01 23:10:43

--- danae/__log__.py
+++ danae/__log__.py
@@ -638,3 +638,19 @@ AttributeError: 'str' object has no attribute 'style'
     self.cena = mesa.acampamento
 AttributeError: 'Mesa' object has no attribute 'acampamento'
 '''},
+{'date': 'Sat Sep 01 2018 20:10:43.108 GMt-0300 (-03) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    STYLE["width"] = 800
+AttributeError: 'module' object has no attribute '__setitem__'
+'''},
